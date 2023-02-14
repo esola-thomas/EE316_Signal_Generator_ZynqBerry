a3051251cb0bd354182e0f03040d7acd6b74bda6
Ernesto Sola-Thomas - schumae <schumae@ad.clarkson.edu>
2023-02-14 15:01:22 -0500

    Start Viviado Project

---
+++ Signal_Generator.srcs/sources_1/new/ADC_user_logic.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/14/2023 03:27:57 PM
+-- Design Name: 
+-- Module Name: ADC_user_logic - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ADC_user_logic is
+    Port ( clk : in STD_LOGIC;
+           scl : out STD_LOGIC;
+           sck : out STD_LOGIC);
+end ADC_user_logic;
+
+architecture Behavioral of ADC_user_logic is
+
+begin
+
+
+end Behavioral;
